5f6bb5551b1479c8ca1fb2287842a7ceaf83ca81
Tilmann Singer <tils@tils.net>
2013-05-07 22:50:50 +0200

    Add validations for name, address and zip

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,9 @@
 class Place < ActiveRecord::Base
   attr_accessible :address, :name, :zip
+
+  validates :address, :name, :presence => true
+
+  validates :zip, :numericality => { :only_integer => true },
+                  :length => { :is => 5 },
+                  :presence => true
 end
