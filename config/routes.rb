6d24e3f294111907226c7eb7ba1a7dad71f98bd0
Tilmann Singer <tils@tils.net>
2013-05-29 22:10:50 +0200

    Add an almost empty root page, instead of the redirect

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Number2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect('/places')
+  root :to => "root#index"
 
   # See how all your routes lay out with "rake routes"
 
